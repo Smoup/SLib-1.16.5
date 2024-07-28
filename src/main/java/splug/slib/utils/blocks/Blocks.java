3fcb3f14f4065c5824dc1eb198e954af44a1efe3
Smoup <gsmorodins@gmail.com>
2024-07-28 23:07:49 +0300

    v3.1.7 | Добавлен Blocks.getNearbyBlocks для получения списка блоков вокруг

---
+++ src/main/java/splug/slib/utils/blocks/Blocks.java
@@ -0,0 +1,24 @@
+package splug.slib.utils.blocks;
+
+import lombok.experimental.UtilityClass;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@UtilityClass @SuppressWarnings("unused")
+public class Blocks {
+    public static Set<Block> getNearbyBlocks(Location center, int radius) {
+        final Set<Block> blocks = new HashSet<>();
+        for (double y = (center.getY() - radius); y <= center.getY() + radius; y++) {
+            for (double z = (center.getZ() - radius); z <= center.getZ() + radius; z++) {
+                for (double x = (center.getX() - radius); x <= center.getX() + radius; x++) {
+                    final Location loc = new Location(center.getWorld(), x, y, z);
+                    blocks.add(loc.getBlock());
+                }
+            }
+        }
+        return blocks;
+    }
+}
