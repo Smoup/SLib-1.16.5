1abac640d6541109efce7daec6250cc8c676c9f8
Smoup <gsmorodins@gmail.com>
2024-07-28 22:45:27 +0300

    v3.1.5 | Исправлен ReplaceBlocks.replaceCubeRadius

--- src/main/java/splug/slib/utils/blocks/ReplaceBlocks.java
+++ src/main/java/splug/slib/utils/blocks/ReplaceBlocks.java
@@ -8,9 +8,9 @@ import org.bukkit.Material;
 public class ReplaceBlocks {
 
     public static void replaceCubeRadius(Location center, int radius, Material from, Material to) {
-        for (int y = (int) (center.getY() - radius); y < center.getY() + radius; y++) {
-            for (int z = (int) (center.getZ() - radius); z < center.getZ() + radius; z++) {
-                for (int x = (int) (center.getX() - radius); x < center.getX() + radius; x++) {
+        for (int y = (int) (center.getY() - radius); y <= center.getY() + radius; y++) {
+            for (int z = (int) (center.getZ() - radius); z <= center.getZ() + radius; z++) {
+                for (int x = (int) (center.getX() - radius); x <= center.getX() + radius; x++) {
                     final Location loc = new Location(center.getWorld(), x, y, z);
                     if (loc.getBlock().getType().equals(from)) loc.getBlock().setType(to);
                 }
