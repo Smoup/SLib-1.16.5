766bb47740f35f43df749961211f49b96954becf
Smoup <gsmorodins@gmail.com>
2024-07-30 20:45:18 +0300

    v3.1.8 | Добавлены методы в PDCUtils для получения информации из PDC

--- src/main/java/splug/slib/utils/items/PDCUtils.java
+++ src/main/java/splug/slib/utils/items/PDCUtils.java
@@ -1,9 +1,13 @@
 package splug.slib.utils.items;
 
+import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 import splug.slib.utils.strings.JsonUtils;
 
 import java.util.Collection;
@@ -18,5 +22,21 @@ public class PDCUtils {
     public static Collection<String> getStringList(PersistentDataContainer pdc, NamespacedKey nKey) {
         final String formatedString = pdc.get(nKey, PersistentDataType.STRING);
         return JsonUtils.stringToStringsCollection(formatedString);
+    }
+
+    @Nullable
+    public static<T> T getData(@NonNull ItemStack item, NamespacedKey nKey, PersistentDataType<?, T> dataType) {
+        if (item.getItemMeta() == null) return null;
+        return item.getItemMeta().getPersistentDataContainer().get(nKey, dataType);
+    }
+
+    @Nullable
+    public static<T> T getData(@NonNull ItemStack item, JavaPlugin plugin, String key, PersistentDataType<?, T> dataType) {
+        return getData(item, new NamespacedKey(plugin, key), dataType);
+    }
+
+    @Nullable
+    public static<T> T getData(@NonNull ItemStack item, String namespace, String key, PersistentDataType<?, T> dataType) {
+        return getData(item, new NamespacedKey(namespace, key), dataType);
     }
 }
