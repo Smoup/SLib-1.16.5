6d7765cf056ca66c6b3e3f2cab71dfb7ccbd3b3f
Smoup <gsmorodins@gmail.com>
2024-08-12 16:05:57 +0300

    v3.3.4 | Добавлен PlayerUtils.setPlayerTotalExperience для установки игроку уровня относительно количества опыта

--- src/main/java/splug/slib/utils/player/PlayerUtils.java
+++ src/main/java/splug/slib/utils/player/PlayerUtils.java
@@ -18,4 +18,32 @@ public class PlayerUtils {
         final Location loc = player.getLocation();
         notGiven.values().forEach(item -> loc.getWorld().dropItem(loc, item));
     }
+
+    public static void setPlayerTotalExperience(@NonNull Player player, int totalExp) {
+        player.setTotalExperience(totalExp);
+
+        final int level;
+        final int expForNextLevel;
+        final float expProgress;
+
+        if (totalExp <= 352) {
+            level = (int) (Math.sqrt(totalExp + 9) - 3);
+            int expAtLevel = level * level + 6 * level;
+            expForNextLevel = 2 * level + 7;
+            expProgress = (float) (totalExp - expAtLevel) / expForNextLevel;
+        } else if (totalExp <= 1507) {
+            level = (int) ((81 / 10.0) + Math.sqrt(2.0 / 5 * (totalExp - 7839.0 / 40)));
+            int expAtLevel = (int) (2.5 * level * level - 40.5 * level + 360);
+            expForNextLevel = 5 * level - 38;
+            expProgress = (float) (totalExp - expAtLevel) / expForNextLevel;
+        } else {
+            level = (int) ((325 / 18.0) + Math.sqrt(2.0 / 9 * (totalExp - 54215.0 / 72)));
+            int expAtLevel = (int) (4.5 * level * level - 162.5 * level + 2220);
+            expForNextLevel = 9 * level - 158;
+            expProgress = (float) (totalExp - expAtLevel) / expForNextLevel;
+        }
+
+        player.setLevel(level);
+        player.setExp(expProgress);
+    }
 }
